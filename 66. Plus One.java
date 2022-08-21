bec6316015e8a8fbab2666e4ed55a3dbb0b38fa1
githubprabh <prabhnoor1998@gmail.com>
2022-08-21 23:45:22 +0530

    Create 66. Plus One.java

---
+++ 66. Plus One.java
@@ -0,0 +1,21 @@
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int[] d= new int[ digits.length + 1 ];
+        
+        for(int i=digits.length -1; i>-1; i--){
+            if(digits[i] != 9){
+                digits[i]++;
+                break;
+            }
+            else{
+                digits[i] = 0;
+            }
+        }
+        if(digits[0] == 0){
+            d[0] = 1;
+            return d;
+        }
+        return digits;
+    }
+}
