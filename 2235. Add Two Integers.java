a354fe8a08f50d94c74fe3d2bdf9b50ddf9a6104
githubprabh <prabhnoor1998@gmail.com>
2022-08-22 22:23:21 +0530

    Create 2235. Add Two Integers.java

---
+++ 2235. Add Two Integers.java
@@ -0,0 +1,10 @@
+Runtime: 1 ms, faster than 67.62% of Java online submissions for Add Two Integers.
+Memory Usage: 41.8 MB, less than 7.49% of Java online submissions for Add Two Integers.
+
+
+
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1+num2;
+    }
+}
